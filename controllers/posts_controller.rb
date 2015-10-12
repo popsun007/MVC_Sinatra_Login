19a59a48d2e32ff7884aafddf96a9d69335e302d
Frank Sun <popsun2010@gmail.com>
2015-10-12 16:12:40 -0700

    Add create post feature

--- controllers/posts_controller.rb
+++ controllers/posts_controller.rb
@@ -1,2 +1,7 @@
 class PostsController < ApplicationController
+  post "/:id" do
+    user_id = params[:id]
+    Post.create(content: params[:Content], user_id: user_id)
+    redirect "/users/#{user_id}"
+  end
 end
---
+++ spec/features/create_post_spec.rb
@@ -0,0 +1,12 @@
+require_relative '../spec_helper'
+RSpec.describe 'creating post' do
+  it 'creates a new post and display the post on the profile page' do
+    user = create_user('Kobe Bryant')
+    sign_in(user)
+    visit "/users/#{user.id}"
+    fill_in 'Content', with: 'This is my post to you'
+    click_button 'Post'
+    expect(current_path).to eq("/users/#{user.id}")
+    expect(page).to have_text('This is my post to you')
+  end
+end
