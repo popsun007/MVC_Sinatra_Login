99001ddfe332f469e95b1a31ba5f26b2f2f4d9f9
Frank Sun <popsun2010@gmail.com>
2015-10-12 07:39:50 -0700

    Add features: display user profile, edit user, delete user

--- controllers/users_controller.rb
+++ controllers/users_controller.rb
@@ -17,6 +17,28 @@ class UsersController < ApplicationController
   post '/new' do
     password_salt = BCrypt::Engine.generate_salt 
     User.create(email: params[:Email], first_name: params[:First_name], last_name: params[:Last_name], salt: password_salt , encrypted_password: BCrypt::Engine.hash_secret(params[:Password], password_salt ))
-    redirect('/users')
+    redirect("/users")
   end
+
+  get '/:id' do
+    @user = User.find(params[:id])
+    erb :'users/show'
+  end
+
+  get '/:id/edit' do
+    @user = User.find(params[:id])
+    erb :'users/edit'
+  end
+
+  post '/:id/edit' do
+    @user = User.find(params[:id])
+    @user.update(email: params[:Email], first_name: params[:First_name], last_name: params[:Last_name])
+    redirect("/users")
+  end
+
+  post '/:id/delete_user' do
+    User.find(params[:id]).destroy
+    redirect("/users")
+  end
+
 end
