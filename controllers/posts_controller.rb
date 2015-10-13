fc06b85eefd143cb318bd50323f9998a6c91e836
Frank Sun <popsun2010@gmail.com>
2015-10-12 17:02:47 -0700

    Add delete post feature for users

--- controllers/posts_controller.rb
+++ controllers/posts_controller.rb
@@ -1,7 +1,22 @@
 class PostsController < ApplicationController
+  
   post "/:id" do
     user_id = params[:id]
     Post.create(content: params[:Content], user_id: user_id)
     redirect "/users/#{user_id}"
   end
+
+  delete "/:id/delete_post" do
+    erb :main_layout do
+      user = User.find(params[:user_id])
+      post = Post.find(params[:id])
+      post.destroy
+      redirect "/users/#{user.id}"
+    end
+    # post_id = params[:id]
+    # user_id = params[:user_id]
+    # Post.find(post_id).destroy
+    # redirect "/users/#{user_id}"
+  end
+
 end
